5d3a410c4e2e48771c5596428a91bb4f3f72907b
Maksim <s1mplexgod1337@gmail.com>
2022-11-27 15:14:26 +0200

    add svg delete todo and created function renderTod

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-let todoItems = []
-
-function addTodo(text) {
-	const todo = {
-		text,
-		checked: false,
-		id: Date.now(),
-	}
-
-	todoItems.push(todo)
-	console.log(todoItems)
-}
-
 const form = document.querySelector('.js-form')
 form.addEventListener('submit', event => {
 	event.preventDefault()
@@ -24,3 +11,36 @@ form.addEventListener('submit', event => {
 		input.focus()
 	}
 })
+
+function renderTodo(todo) {
+	const todoList = document.querySelector('.todos')
+	const ischecked = todo.checked ? 'done' : ''
+	const node = document.createElement('li')
+
+	node.setAttribute('class', `todo-item ${ischecked}`)
+	node.setAttribute('data-key', todo.id)
+
+	node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `
+
+	todoList.append(node)
+}
+
+let todoItems = []
+
+function addTodo(text) {
+	const todo = {
+		text,
+		checked: false,
+		id: Date.now(),
+	}
+
+	todoItems.push(todo)
+	renderTodo(todo)
+}
