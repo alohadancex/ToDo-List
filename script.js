22e5104f07eafd78e3a6b927abf7301e0f3d8cb9
Maksim <s1mplexgod1337@gmail.com>
2022-11-27 16:25:26 +0200

    add function remove items and marks times if done

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const todoList = document.querySelector('.todos')
+todoList.addEventListener('click', event => {
+	if (event.target.classList.contains('js-tick')) {
+		const itemKey = event.target.parentElement.dataset.key
+		toggleDone(itemKey)
+	}
+
+	//Delete task from todo
+	if (event.target.classList.contains('js-delete-todo')) {
+		const itemKey = event.target.parentElement.dataset.key
+		deleteTodo(itemKey)
+	}
+})
+
 const form = document.querySelector('.js-form')
 form.addEventListener('submit', event => {
 	event.preventDefault()
@@ -12,26 +26,6 @@ form.addEventListener('submit', event => {
 	}
 })
 
-function renderTodo(todo) {
-	const todoList = document.querySelector('.todos')
-	const ischecked = todo.checked ? 'done' : ''
-	const node = document.createElement('li')
-
-	node.setAttribute('class', `todo-item ${ischecked}`)
-	node.setAttribute('data-key', todo.id)
-
-	node.innerHTML = `
-    <input id="${todo.id}" type="checkbox"/>
-    <label for="${todo.id}" class="tick js-tick"></label>
-    <span>${todo.text}</span>
-    <button class="delete-todo js-delete-todo">
-    <svg><use href="#delete-icon"></use></svg>
-    </button>
-  `
-
-	todoList.append(node)
-}
-
 let todoItems = []
 
 function addTodo(text) {
@@ -43,4 +37,54 @@ function addTodo(text) {
 
 	todoItems.push(todo)
 	renderTodo(todo)
+}
+// Mark task a complate
+function deleteTodo(key) {
+	const index = todoItems.findIndex(item => item.id === Number(key))
+	const todo = {
+		deleted: true,
+		...todoItems[index],
+	}
+	//remove the todo item from array;
+	todoItems = todoItems.filter(item => item.id !== Number(key))
+	console.log(key)
+	renderTodo(todo)
+}
+
+function renderTodo(todo) {
+	const todoList = document.querySelector('.todos')
+	const item = document.querySelector(`[data-key='${todo.id}']`)
+
+	if (todo.deleted) {
+		item.remove()
+		return
+	}
+
+	const isChecked = todo.checked ? 'done' : ''
+	const node = document.createElement('li')
+
+	node.setAttribute('class', `todo-item ${isChecked}`)
+	node.setAttribute('data-key', todo.id)
+
+	node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `
+
+	if (item) {
+		todoList.replaceChild(node, item)
+	} else {
+		todoList.append(node)
+	}
+}
+
+function toggleDone(key) {
+	const index = todoItems.findIndex(item => item.id === Number(key))
+
+	todoItems[index].checked = !todoItems[index].checked
+	renderTodo(todoItems[index])
 }
